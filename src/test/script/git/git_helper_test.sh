37bb096d9527513a1a8ce22bbdb14d1c70058dab
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-01-27 23:24:53 +0100

    Enhanced test script for git_helper.sh

--- src/test/script/git/git_helper_test.sh
+++ src/test/script/git/git_helper_test.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
+# Script setup, in case of error will exit immediately
+set -o errexit
+set -o pipefail
+
 rm -fr fj-*
 
 TEST_FILE=fj-doc/pom.xml
 
-../../../../src/main/script/git/git_helper.sh -k clone -s core -j ../../../../src/main/script/git/repo-subset.json
+REPO_SUBSET=../../../../src/main/script/git/repo-subset.json
+
+SCRIPT=../../../../src/main/script/git/git_helper.sh 
+
+# built in command : clone
+${SCRIPT} -k clone -s core -j ${REPO_SUBSET}
+# built in command : fetch, checkout develop, pull
+${SCRIPT} -k checkout -b develop -s core -j ${REPO_SUBSET}
+# custom command : git log -3
+${SCRIPT} -c 'git log -3'  -s core -j ${REPO_SUBSET}
 
 if [[ -f ${TEST_FILE} ]]; then
 	echo "${TEST_FILE} exists! test OK"
